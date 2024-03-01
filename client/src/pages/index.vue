c79cab1f542b8bd3e1ccc2280423f3603537d2fa
dragiev1 <dragievs1@newpaltz.edu>
2024-02-29 19:07:27 -0500

    Fixed Hero in Index

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -3,11 +3,11 @@
 </script>
 
 <template>
-  <main class = "Hero is primary is large">
-    <div class = "hero body">
+  <main class="hero is-primary is-large">
+    <div class="hero-body">
       <div class="container">
-        <h1 class="Title">Welcome to the home page</h1>
-        <h2 class="Subtitle">text</h2>
+        <h1 class="title">Welcome to the home page</h1>
+        <h2 class="subtitle">text</h2>
       </div>
     </div>
   </main>
