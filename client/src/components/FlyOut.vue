c161947eb438b5d91ddcfc4ad6469f57680a2213
dragiev1 <dragievs1@newpaltz.edu>
2024-03-11 14:58:28 -0400

    Created Flyout Component

---
+++ client/src/components/FlyOut.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        isOpen?: boolean
+    }>()
+</script>
+
+<template>
+    <div class="flyout" :class="{open: isOpen}">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+    .flyout {
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 20rem;
+        height: 100%;
+        background-color: lightgray;
+        border: 1px solid gray;
+        padding: 1rem;
+        z-index: 100;
+        box-shadow: -1px 0 5px 10 rgba(0, 0, 0, 0.25);
+        transform: translateX(80%);
+        transition: transform 1s;
+    }
+    .flyout.open, .flyout:hover {
+        transform: translateX(0);
+    }
+    .price {
+        font-weight: bold;
+        font-size: xx-large;
+        color: red;
+        float: right;
+    }
+</style>
--- client/src/pages/products/index.vue
+++ client/src/pages/products/index.vue
@@ -2,6 +2,7 @@
     import {ref, computed} from 'vue'
     import { type Product, getProducts } from "@/model/products";
     import ProductCard from '@/components/ProductCard.vue';
+    import FlyOut from '@/components/FlyOut.vue';
     
     
     const products = ref([] as Product[])
@@ -37,7 +38,7 @@
                     />
     </div>
 
-    <div class="flyout">
+    <FlyOut :isOpen="false">
         <h1 class="title">
             The Cart
         </h1>
@@ -48,7 +49,7 @@
             </li>
         </u1>
         {{ cart.length }} items; total: ${{ total }}
-    </div>
+    </Flyout>
     
 </template>
 
@@ -58,46 +59,11 @@
         flex-wrap: wrap;
         justify-content: space-around;
     }
-    .card {
-        flex-basis: 15rem;
-        flex-grow: 1;
-        margin: 0.5rem;
-        box-shadow: 3px 3px 10 rgba(0, 0, 0, 0.25);
-    }
-    h3 {
-        font-weight: bold;
-    }
-    .price {
-        font-weight: bold;
-        font-size: xx-large;
-        color: red;
-        float: right;
-    }
-    .flyout {
-        position: fixed;
-        top: 0;
-        right: 0;
-        width: 20rem;
-        height: 100%;
-        background-color: lightgray;
-        border: 1px solid gray;
-        padding: 1rem;
-        z-index: 100;
-        box-shadow: -1px 0 5px 10 rgba(0, 0, 0, 0.25);
-        transform: translateX(80%);
-        transition: transform 1s;
-    
-    }
-    .flyout.open, .flyout:hover {
-        transform: translateX(0);
-    }
-
     .cart li {
         display: flex;
         align-items: center;
         margin: 1rem 0;
     }
-
     .cart img {
         width: 3rem;
         height: 3rem;
